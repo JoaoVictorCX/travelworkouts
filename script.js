5d5222e92e3d515b98c1fa949676a09b3474c605
JoaoVictorCX <joaovictorcoelhox@gmail.com>
2023-08-19 16:14:53 -0300

    incial

---
+++ script.js
@@ -0,0 +1,48 @@
+let lower = [
+"Squat",
+"Alt. Lunge",
+"Step-up",
+"Hip Extension",
+"Bird Dog"
+]
+
+let cardio = [
+"Burpee",
+"Sprawls",
+"Double-under",
+"Shuttlen run",
+"Jumping Jack"
+]
+
+
+let res = document.querySelector('div#res')
+
+function wodgen () {
+    res.innerHTML = ''
+    res.innerHTML += '10 rounds for time of'
+    randMovements = []
+    
+    //FUNÇÃO SORTEAR E INSERIR NUMEROS SORTEADOS
+    while (randMovements.length < 3){
+        var num = Math.floor(Math.random() * 5) + 1
+        if (randMovements.indexOf(num) === -1){
+            randMovements.push(num)
+        }
+    }
+    //FUNÇÃO VERIFICAR NIVEL ATIVIDADE (5/10/15 REPS)
+
+    //FUNÇÃO IMPRIMIR OU SORTEAR DE ACORDO COM TIPO (UPPER/LOWER...)    
+
+    //FUNÇÃO IMPRIMIR MOVIMENTOS CORRESPONDENTES AO NUMERO SORTEADO
+    for (let i = 0; i < (randMovements.length); i++){
+        var numMov = randMovements[i]
+        if (i < 2){
+            var movement = lower[numMov - 1]
+            res.innerHTML += `<p>${movement} x 10 reps`
+        } else {
+            var movement = cardio[numMov - 1]
+            res.innerHTML += `<p>${movement} x 10 reps`
+        }
+    }
+
+}
