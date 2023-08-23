9377d46e51a2a5ad76d7261644d8d8c7218bec0d
JoaoVictorCX <joaovictorcoelhox@gmail.com>
2023-08-23 17:55:39 -0300

    form-up

--- script.js
+++ script.js
@@ -15,33 +15,49 @@ const cardio = [
 ]
 
 let res = document.querySelector('div#res')
+let param = (new URL(document.location)).searchParams
+let type = param.get('type')
+let level = param.get('level')
+let reps = 0
+
+const niveis = {
+    beginner:5, 
+    intermed:10, 
+    advanced:15, 
+    athlet:20
+}
+
+if (Object.hasOwn(niveis, level)){
+    reps = niveis[level]
+}
 
 function wodgen () {
     res.innerHTML = ''
     res.innerHTML += '<p><strong>10 rounds for time:<br>'
     randMovements = []
-    
-    //FUNÇÃO SORTEAR E INSERIR NUMEROS SORTEADOS
+
     while (randMovements.length < 3){
         var num = Math.floor(Math.random() * 5) + 1
         if (randMovements.indexOf(num) === -1)
             randMovements.push(num)
     }
-    //FUNÇÃO VERIFICAR NIVEL ATIVIDADE (5/10/15 REPS)
 
-    //FUNÇÃO IMPRIMIR OU SORTEAR DE ACORDO COM TIPO (UPPER/LOWER...)    
-
-    //FUNÇÃO IMPRIMIR MOVIMENTOS CORRESPONDENTES AO NUMERO SORTEADO
     for (let i = 0; i < (randMovements.length); i++){
-        var numMov = randMovements[i]
-        
+        var numMov = randMovements[i]       
         if (i < 2){
             var movement = lower[numMov - 1]
-            res.innerHTML += `${movement} x 10 reps<br>`
+            res.innerHTML += `${movement} x ${reps} reps<br>`
         } else {
             var movement = cardio[numMov - 1]
-            res.innerHTML += `${movement} x 10 reps<br>`
+            res.innerHTML += `${movement} x ${reps} reps<br>`
         }
     }
 
 }
+
+function setURL () {
+    let param2 = document.location.search
+    let btnovo = document.getElementById('btnpage3')
+    btnpage3.href = "page6.html"+param2
+}
+
--- script6.js
+++ script6.js
@@ -1,7 +1,28 @@
 
-let param2 = document.location.search
-let btngo = document.getElementById('imgbtngo')
+let param = (new URL(document.location)).searchParams
+let type = param.get('type')
+let level = param.get('level')
 
-function setURL () {
-    imgbtngo.href = param2
+const tipos = {
+    fullbody:'1', 
+    upper:'2', 
+    lower:'3', 
+    abs:'4'
+}
+
+if (Object.hasOwn(tipos, type)){
+    let res = document.getElementById('restype')
+    res.innerHTML = tipos[type]
+}
+
+const niveis = {
+    beginner:'inic', 
+    intermed:'inter', 
+    advanced:'avan', 
+    athlet:'atle'
+}
+
+if (Object.hasOwn(niveis, level)){
+    let res = document.getElementById('reslevel')
+    res.innerHTML = niveis[level]
 }
