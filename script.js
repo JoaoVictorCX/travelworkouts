4050ff5f3b11b9798eb04c9491979e46095faa89
JoaoVictorCX <joaovictorcoelhox@gmail.com>
2023-08-26 18:43:33 -0300

    função

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
-
-
-const lowermovs = {
+const lower = {
     squat: {
         nome: "air squats",
-        qtde: 20
+        qtde: 40
     },
     lunge: {
         nome: "lunges",
-        qtde: 20
+        qtde: 40
     },
     step: {
         nome: "step-ups",
-        qtde: 20
+        qtde: 40
     },
     hipextension: {
-        nome: "hip entension",
-        qtde: 20
+        nome: "hip extension",
+        qtde: 40
     },
     downup: {
         nome: "jumping squats",
+        qtde: 40
+    }
+}
+
+const upper = {
+    pushup: {
+        nome: "push-ups",
+        qtde: 20
+    },
+    shouldertaps: {
+        nome: "shoulder taps",
+        qtde: 40
+    },
+    hspu: {
+        nome: "handstand push-ups",
+        qtde: 16
+    },
+    pullup: {
+        nome: "pull-ups",
+        qtde: 20
+    },
+    dips: {
+        nome: "static dips",
         qtde: 20
     }
 }
 
-const abs = [
-    "Sit-up",
-    "V-up alt",
-    "V-up",
-    "Plank",
-    "Mountain climbers"
-]
+const abs = {
+    situp: {
+        nome: "sit-ups",
+        qtde: 32
+    },
+    mountainclimber: {
+        nome: "mountain climbers",
+        qtde: 48
+    },
+    vupalt: {
+        nome: "alt. v-ups",
+        qtde: 24
+    },
+    sideplank: {
+        nome: "bicycle Crunch",
+        qtde: 32
+    },
+    plank: {
+        nome: "secs plank",
+        qtde: 60
+    }
+}
 
-const cardiomovs = {
+const cardio = {
     burpee: {
         nome: "burpees",
         qtde: 20
@@ -50,7 +86,7 @@ const cardiomovs = {
     },
     run: {
         nome: "meters run",
-        qtde: 800
+        qtde: 400
     }
 }
 
@@ -73,7 +109,7 @@ if (Object.hasOwn(niveis, level)){
 
 function wodgen () {
     res.innerHTML = ''
-    res.innerHTML += '<strong>10 rounds for time:'
+    res.innerHTML += '<strong>5 rounds for time:'
     randMovements = []
     
     while (randMovements.length < 3){
@@ -84,17 +120,59 @@ function wodgen () {
 
     for (let i = 0; i < (randMovements.length); i++){
         var numMov = randMovements[i]
+
         if (i < 1){
-            cardioKeys = Object.keys(cardiomovs)
+            cardioKeys = Object.keys(cardio)
             let movement = cardioKeys[numMov - 1]
-            let movimento = cardiomovs[movement]
+            let movimento = cardio[movement]
+            res.innerHTML += `${movimento.qtde*reps} ${movimento.nome}<br>`
+        } else { 
+            if (type == 'fullbody') {
+                i--
+                lowerKeys = Object.keys(lower)
+                let movement = lowerKeys[numMov - 1]
+                let movimento = lower[movement]
+                res.innerHTML += `${movimento.qtde*reps} ${movimento.nome}<br>`
+                i++
+                upperKeys = Object.keys(upper)
+                let movement2 = upperKeys[numMov - 1]
+                let movimento2 = upper[movement2]
+                res.innerHTML += `${movimento2.qtde*reps} ${movimento2.nome}<br>`
+                i++
+                absKeys = Object.keys(abs)
+                let movement3 = absKeys[numMov - 1]
+                let movimento3 = abs[movement3]
+                res.innerHTML += `${movimento3.qtde*reps} ${movimento3.nome}<br>`
+            } else if (type == 'lower') {
+                lowerKeys = Object.keys(lower)
+                let movement = lowerKeys[numMov - 1]
+                let movimento = lower[movement]
+                res.innerHTML += `${movimento.qtde*reps} ${movimento.nome}<br>`
+            } else if  (type == 'upper') {
+                upperKeys = Object.keys(upper)
+                let movement = upperKeys[numMov - 1]
+                let movimento = upper[movement]
+                res.innerHTML += `${movimento.qtde*reps} ${movimento.nome}<br>`
+            } else if (type == 'abs') {
+                absKeys = Object.keys(abs)
+                let movement = absKeys[numMov - 1]
+                let movimento = abs[movement]
+                res.innerHTML += `${movimento.qtde*reps} ${movimento.nome}<br>`
+            }
+        }
+
+        /*
+        if (i < 1){
+            cardioKeys = Object.keys(cardio)
+            let movement = cardioKeys[numMov - 1]
+            let movimento = cardio[movement]
             res.innerHTML += `${movimento.qtde*reps} ${movimento.nome}<br>`
         } else {
-            lowerKeys = Object.keys(lowermovs)
+            lowerKeys = Object.keys(lower)
             let movement = lowerKeys[numMov - 1]
-            let movimento = lowermovs[movement]
+            let movimento = lower[movement]
             res.innerHTML += `${movimento.qtde*reps} ${movimento.nome}<br>`
-        }
+        } */
     }
 }
 
